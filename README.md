5d93e7d4a0fa67f3d21094b99d3116f65dbb91aa
amazing-shubham <smartshubhamgupta123@gmail.com>
2020-08-04 19:05:26 +0530

    README.md motified and few functions added in the RPS game

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ## Building mini-projects using Python-
 ### 1. Simple Calculator
 In this mini project you can add simple calculation like add, sub, div, mul, square, cube and along with this, there are basic calculator functions like clear all, delete, etc.
+
+### 2. Rock, Paper & Scissors Game
+In this project, you can play this game. Everyone in their childhood aware of this game.
+
--- Rock_Paper_Scissor_Game.py
+++ Rock_Paper_Scissor_Game.py
@@ -2,7 +2,7 @@ from tkinter import *
 import random
 
 root = Tk()
-root.geometry("500x350")
+root.geometry("500x375")
 root.title('Rock Paper Scissors')
 # creating new frame
 newframe = Frame(root)
@@ -17,23 +17,87 @@ comp_choice = ""
 # convert choice to number
 def conversion_choice_to_number(choice):
     a = {'Rock': 0, 'Paper': 1, 'Scissors': 2}
-    return s[choice]
+    return a[choice]
 
 
 # convert number to choice
 def conversion_number_to_choice(number):
     a = {0: 'Rock', 1: 'Paper', 2: 'Scissors'}
-    return s[number]
+    return a[number]
+
 
 # random selection by computer
 def random_generation():
     list = ['Rock', 'Paper', 'Scissors']
     selection = random.choice(list)
-    computerselection['text'] = selection
+    computer_selection['text'] = selection
     return selection
 
+
 # function for play
-def play():
+def play(comp_choice1, user_choice1):
+    try:
+        global user_score
+        global comp_score
+        global count
+        user = conversion_choice_to_number(user_choice1)
+        comp = conversion_choice_to_number(comp_choice1)
+        if user == comp:
+            textArea2.delete(1.0, END)
+            textArea2.insert(END, "This round is TIE")
+
+        elif (user - comp) % 3 == 1:
+            user_score += 1
+            your_score['text'] = user_score
+            textArea2.delete(1.0, END)
+            textArea2.insert(END, "This round is won by user")
+        else:
+            comp_score += 1
+            comp_score1['text'] = comp_score
+            textArea2.delete(1.0, END)
+            textArea2.insert(END, "This round is won by Computer")
+    except Exception:
+        textArea2.delete(1.0, END)
+        textArea2.insert(END, "ERROR")
+
+
+# function of rock button
+def ROCK():
+    global user_choice
+    global comp_choice
+    user_choice = 'Rock'
+    comp_choice = random_generation()
+    play(comp_choice, user_choice)
+
+
+# function of Paper button
+def PAPER():
+    global user_choice
+    global comp_choice
+    user_choice = 'Paper'
+    comp_choice = random_generation()
+    play(comp_choice, user_choice)
+
+
+# function of rock button
+def SCISSORS():
+    global user_choice
+    global comp_choice
+    user_choice = 'Scissors'
+    comp_choice = random_generation()
+    play(comp_choice, user_choice)
+
+
+# reset button
+def reset():
+    global user_score
+    global comp_score
+    user_score = 0
+    comp_score = 0
+    comp_score1['text'] = 0
+    your_score['text'] = 0
+    textArea2.delete(1.0, END)
+    computer_selection['text'] = "*******"
 
 
 # Adding label of the frame
@@ -41,25 +105,25 @@ label = Label(newframe, text="Select any one of the following options:", anchor=
 label.pack()
 
 # adding buttons for rock, paper and scissors
-rockButton = Button(newframe, text="ROCK", bg="Light Blue", command=lambda: random_generation())
+rockButton = Button(newframe, text="ROCK", bg="Light Blue", command=lambda: ROCK())
 rockButton.pack(padx=40, pady=10)
-paperButton = Button(newframe, text="PAPER", bg="Yellow", command=lambda: random_generation())
+paperButton = Button(newframe, text="PAPER", bg="Yellow", command=lambda: PAPER())
 paperButton.pack(padx=40, pady=10)
-scissorButton = Button(newframe, text="SCISSORS", bg="Light Green", command=lambda: random_generation())
+scissorButton = Button(newframe, text="SCISSORS", bg="Light Green", command=lambda: SCISSORS())
 scissorButton.pack(padx=40, pady=10)
 
 # adding textArea and inner content
 textArea = Text(newframe, bg='peachpuff', height=20, width=50, relief='ridge')
 textArea.pack()
 
-insideTextArea = Label(textArea, text="Enter no. of round you wants to play:", anchor='w', bg='peachpuff')
-insideTextArea.grid(row=0, column=0)
-box = Entry(textArea)
-box.grid(row=0, column=1)
+# insideTextArea = Label(textArea, text="Enter no. of round you wants to play:", anchor='w', bg='peachpuff')
+# insideTextArea.grid(row=0, column=0)
+# box = Entry(textArea)
+# box.grid(row=0, column=1)
 
 inside_TextArea2 = Label(textArea, text="Computer Selected:", anchor='w', bg='peachpuff')
 inside_TextArea2.grid(row=2, column=0)
-computer_selection = Label(textArea, bg='peachpuff')
+computer_selection = Label(textArea, text="*******", bg='peachpuff')
 computer_selection.grid(row=2, column=1)
 
 inside_TextArea4 = Label(textArea, text="Your's Score:", anchor='w', bg='peachpuff')
@@ -69,10 +133,16 @@ your_score.grid(row=4, column=1)
 
 inside_TextArea6 = Label(textArea, text="Computer's Score:", anchor='w', bg='peachpuff')
 inside_TextArea6.grid(row=6, column=0)
-comp_score = Label(textArea, bg='peachpuff', text='0')
-comp_score.grid(row=6, column=1)
+comp_score1 = Label(textArea, bg='peachpuff', text='0')
+comp_score1.grid(row=6, column=1)
 
-textArea2 = Text(newframe, bg='sandybrown', height=2, width=15, relief='ridge')
+textArea2 = Text(newframe, bg='sandybrown', height=2, width=30, relief='ridge')
 textArea2.pack()
+
+resetButton = Button(newframe, text="RESET", bg="Red", command=lambda: reset())
+resetButton.pack(padx=40, pady=10)
+
+quitButton = Button(newframe, text="QUIT", bg="Green", command=lambda: newframe.quit())
+quitButton.pack(padx=40, pady=10)
 
 root.mainloop()
